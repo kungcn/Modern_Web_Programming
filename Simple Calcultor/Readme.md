92fe647164d4f2eab594d217bf83c00182f81527
kungcn <kcnnow@gmail.com>
2015-11-08 09:41:38 +0800

    week 07 homework

---
+++ Simple Calcultor/Readme.md
@@ -0,0 +1,5 @@
+# Readme
+  This is the sixth week homework of Modern Web Programming.
+
+## Update
+1. calcultor finished generally.
---
+++ Simple Calcultor/js/calculator.js
@@ -0,0 +1,85 @@
+window.onload = function() {
+
+	//  get all buttons
+	var arr = document.getElementsByTagName("button");
+	for (var i = 0; i < arr.length; i++)
+		//  when clicking the following buttons, innerHTML of input changes
+        //  CE,<- and =, although they are in the array, when clicking them, the following codes will be covered
+		arr[i].onclick = function() {
+			var temp = document.getElementById("input");
+			if (temp.value == "")
+                temp.value = this.innerHTML + "";
+            else
+			    temp.value = temp.value + this.innerHTML + "";
+		}
+
+	//  when clicking CE, clear all input
+	document.getElementById("clearAll").onclick = function() {
+		document.getElementById("input").value = "";
+    }
+
+    //  when clicking <-, clear the last input
+    document.getElementById("clearOne").onclick = function() {
+    	document.getElementById("input").value = document.getElementById("input").value.substring(0,document.getElementById("input").value.length - 1);
+    }
+
+    //  when clicking =, check and calculate the formula later
+    document.getElementById("equality").onclick = function() {
+
+        var left_append = [], right_append = [];
+        var opeartor = ["+", "-", "*", "/"];
+
+        for (var i = 0; i < document.getElementById("input").value.length; i++) {
+        	//  eval() can not do operation when any operator is omitted
+        	if (document.getElementById("input").value.charAt(i) == "(" && !isNaN(document.getElementById("input").value.charAt(i - 1))) {
+        		alert("Syntax Error, can not omit opeartors before \"\(\" !");
+        		return 0;
+        	}
+
+            //  prevent repeated operators, both * and /
+        	if (document.getElementById("input").value.charAt(i) == "*") {
+        		for (var j = 0; j < opeartor.length; j++)
+        			if (document.getElementById("input").value.charAt(i - 1) == opeartor[j]) {
+        				alert("Syntax Error, can not input operators repeatedly!");
+        				return 0;
+        			}
+        	}
+        	if (document.getElementById("input").value.charAt(i) == "/") {
+        		for (var j = 0; j < opeartor.length; j++)
+        			if (document.getElementById("input").value.charAt(i - 1) == opeartor[j]) {
+        				alert("Syntax Error, can not input operators repeatedly!");
+        				return 0;
+        			}
+        	}
+
+            //  no more than one .
+            if (document.getElementById("input").value.charAt(i) == "." && document.getElementById("input").value.charAt(i - 1) == ".") {
+                alert("Syntax Error, more than one \".\"!");
+                return 0;
+            }
+        	else if (document.getElementById("input").value.charAt(i) == "(")
+        		left_append.push(i);
+        	else if (document.getElementById("input").value.charAt(i) == ")")
+        		right_append.push(i);
+        }
+
+        //  numbers of () is equal or not
+        if (left_append.length != right_append.length) {
+        	alert("Syntax Error, lose \"(\" or \")\"!");
+        } else {
+            try  {
+                var temp = eval(document.getElementById("input").value).toFixed(8);
+            }
+            catch(exception) {
+                alert("Unknown Syntax Error, check again please!");
+                return 0;
+            }
+            if (temp == "Infinity") {
+                //  math error includes divide by 0
+                alert("Math Error, check again please!");
+            } else {
+                document.getElementById("input").value = temp;
+            }
+        }
+    }
+}
